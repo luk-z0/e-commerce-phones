546ae5f9fd0ec31203dab76aca2e505aead6a49a
Lucas Luz <lucasdias200lucasdias@gmail.com>
2025-10-01 17:27:51 -0300

    fix: rename AppHeader to AppNav

--- app/components/AppNav.vue
+++ app/components/AppNav.vue
@@ -22,35 +22,22 @@
         }}</a>
       </nav>
 
-      <!-- Seletor de Idioma -->
-      <div class="relative md:flex">
-        <UInputMenu
-          v-model="selectedLocale"
-          :items="localeItems"
-          value-key="value"
-          @update:model-value="changeLocale"
-        />
-      </div>
+      <aside class="flex items-center">
+        <!-- Seletor de Idioma -->
+        <div class="relative">
+          <UInputMenu v-model="selectedLocale" :items="localeItems" value-key="value" @update:model-value="changeLocale"
+            size="sm" />
+        </div>
 
-      <!-- Botão de Menu (visível em telas pequenas) -->
-      <button
-        class="md:hidden text-gray-500 hover:text-gray-700 focus:outline-none"
-      >
-        <svg
-          class="h-6 w-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path>
-        </svg>
-      </button>
+        <!-- User Dropdown -->
+        <ClientOnly v-if="loggedIn">
+          <div class="ml-4">
+            <LayoutUserDropdown />
+          </div>
+        </ClientOnly>
+        <UAvatar v-else class="ml-4 cursor-pointer" size="md" src="https://avatar.iran.liara.run/public"
+          @click="$router.push('/login')" :alt="$t('header.login')" />
+      </aside>
     </div>
   </header>
 </template>
@@ -60,6 +47,7 @@ import { ref, computed, toRef } from "vue";
 import { useI18n } from "vue-i18n";
 
 const { locale, locales, setLocale } = useI18n();
+const { loggedIn } = useUserSession();
 
 const selectedLocale = ref(toRef(locale, "value"));
 
