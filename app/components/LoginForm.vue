0c15b061b7d2aee18b57017d1d15559557c6c7f9
Lucas Luz <lucasdias200lucasdias@gmail.com>
2025-10-03 18:40:40 -0300

    refactor: add google icon to login button remove: Icon component

--- app/components/LoginForm.vue
+++ app/components/LoginForm.vue
@@ -2,8 +2,10 @@
   <UContainer>
     <UButton color="neutral" href="/auth/google" rel="noopener noreferrer" block
       class="justify-center text-xl font-semibold">
-      <Icon name="google" class="w-8 " />
-      {{ $t("login.google") }}
+      <span class="flex items-center">
+        <Icon name="logos:google-icon" class="w-8 " />
+        <p class="text-lg font-medium text-black">{{ $t("login.google") }}</p>
+      </span>
     </UButton>
   </UContainer>
 </template>
